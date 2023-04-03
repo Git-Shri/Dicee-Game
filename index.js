457ef39023fa5e9442e46087a5a685bae2721977
Git-Shri <101913150+Git-Shri@users.noreply.github.com>
2023-04-03 19:48:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1=Math.floor((Math.random() * 6)+1);
 // document.querySelector.().getAttributes
-var randomDiceImg = "dice" +randomNumber1 +".png";
-var randomImgSource = "images/" +randomDiceImg;
+
+var randomImgSource = "dice" +randomNumber1 +".png";
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImgSource);
@@ -10,8 +10,8 @@ image1.setAttribute("src", randomImgSource);
 
 
 var randomNumber2 =Math.floor((Math.random() * 6)+1);
-var randomDiceImg2 = "dice"+ randomNumber2 + ".png";
-var randomImgSource2 = "images/" + randomDiceImg2;
+
+var randomImgSource2 = "dice"+ randomNumber2 + ".png";
 
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImgSource2);
